f1fbd73456427c4a6893385ae5c3bb814ae39898
Luis López <ldotlopez@gmail.com>
2020-08-31 08:41:16 +0200

    black formatting

--- cs/pharmacies/__main__.py
+++ cs/pharmacies/__main__.py
@@ -7,17 +7,18 @@ import logging
 
 from cs import pharmacies
 
-LOGGER = logging.getLogger('cs.pharmacies')
+LOGGER = logging.getLogger("cs.pharmacies")
+
 
 def main():
     logging.basicConfig()
     LOGGER.setLevel(logging.DEBUG)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--city-code', required=True)
-    parser.add_argument('--output-file', required=False)
-    parser.add_argument('--force-empty-output-file', action='store_true')
-    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument("--city-code", required=True)
+    parser.add_argument("--output-file", required=False)
+    parser.add_argument("--force-empty-output-file", action="store_true")
+    parser.add_argument("--verbose", action="store_true")
 
     args = parser.parse_args()
 
@@ -25,19 +26,17 @@ def main():
 
     now = datetime.datetime.now()
     if now.hour == 21 and now.minute >= 30:
-        data.extend(
-            get_pharmacies(now=now + datetime.timedelta(days=1))
-        )
+        data.extend(get_pharmacies(now=now + datetime.timedelta(days=1)))
 
     data = {
-        '_updated': now.timestamp(),
-        'pharmacies': [x.asdict() for x in data]
+        "_updated": now.timestamp(),
+        "pharmacies": [x.asdict() for x in data],
     }
     jsondata = json.dumps(data, indent=2)
 
     if args.output_file:
         if data and not args.force_empty_output_file:
-            with open(args.output_file, 'w+', encoding='utf-8') as fh:
+            with open(args.output_file, "w+", encoding="utf-8") as fh:
                 fh.write(jsondata)
         else:
             logmsg = "Refusing to save empty data"
@@ -45,6 +44,7 @@ def main():
 
     if args.verbose:
         print(jsondata)
+
 
 if __name__ == "__main__":
     main()
